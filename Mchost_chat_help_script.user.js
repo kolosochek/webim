0cf36b6220f059f14492dedb939bb7bd45b0fbed
kolosochek <kolosochek@gmail.com>
2015-11-16 15:09:38 +0500

    added autorespond feature; TODO: move autorespond feature to complete.js + regexp on joxi links replace works only once, add google DIG feature

--- Mchost_chat_help_script.user.js
+++ Mchost_chat_help_script.user.js
@@ -34,6 +34,9 @@ var defaultExtension = 'jpg';
 var defaultQASearchAnchor = '!найди';
 var defaultMchelpSearchAnchor = "!помощь";
 var defaultWhoisSearchAnchor = "!whois";
+// Set GMT difference with MOSCOW GMT for proper autorespond feature work
+var GMT_difference = "+2"; //TMN +2 
+
 // type text in textarea
 textarea.addEventListener('input', function(){
 	// TODO: increase perfomance
@@ -132,7 +135,7 @@ textarea.addEventListener('input', function(){
 	            var ns = jsonWhois.nameservers;
 
 	            // prepend MSHOST WHOIS LINK
-	            content+= "http://mchost.ru/whois/?domainName=" + query + separator
+	            content+= "DNS записи для домена " + query + " http://mchost.ru/whois/?domainName=" + query + separator
 
 	            // NS
 	            content+= "NS: ";
@@ -255,17 +258,114 @@ function toggle_modal_window(content){
 // autoreply functions
 // autoreply 10 min afk
 function get_last_message_timestamp(){
-var content = document.getElementById("chatwnd").contentWindow.document.getElementById("content");
-var span = content.querySelectorAll("span");
-var current_date = new Date();
-var last_event_timestamp;
+	var content = document.getElementById("chatwnd").contentWindow.document.getElementById("content");
+	var span = content.querySelectorAll("span");
+	var last_event_timestamp;
   	for (i=span.length-1;i>0;i--){
-  		console.log(i);
   		if(span[i].className == ""){
-  			console.log(span[i]);
-  			last_event_timestamp = span[i];
-  			//break();
+  			last_event_timestamp = span[i].textContent;
+  			break;
   		}
   	}
+	if (typeof(last_event_timestamp) == 'string'){
+		last_event_timestamp = last_event_timestamp.split(':');
+		if (last_event_timestamp.length == 3){
+			var object_params = {
+				'hours': parseInt(last_event_timestamp[0]),
+				'minutes': parseInt(last_event_timestamp[1]),
+				'seconds': parseInt(last_event_timestamp[2]),
+			}
+			return new Object(object_params);
+		} else {
+			console.log('wrong last message timestamp split');
+			console.log(last_event_timestamp);
+		}
+	} else {
+		return false	
+	}
+}
+var GMT_difference = "+2"; //TMN +2 
+var close_dialog_interval = 10;
+var last_message_object = get_last_message_timestamp();
+
+function get_current_time_object(){
+	var current_date = new Date();	
+	var current_date_object = {
+		'hours': 0,//parseInt(current_date.getHours())-GMT_difference,
+		'minutes': parseInt(current_date.getMinutes()),
+		'seconds': parseInt(current_date.getSeconds()),
+	}
+	if (GMT_difference.search(/\+/g)+1){
+		current_date_object.hours = current_date.getHours() - parseInt(GMT_difference);
+	} else if(GMT_difference.search('-')+1){
+		current_date_object.hours = current_date.getHours() + parseInt(GMT_difference);
+	} else {
+		console.log("can't set current hours value with GMT difference");
+		current_date_object.hours = 0;
+	}
+	return current_date_object
+}
+function compare_time(current_time, last_message_time){
+	// compare hours
+	if (current_time.hours == last_message_time.hours){
+		console.log('hours are matching');
+		// compare minutes
+		if(current_time.minutes >= last_message_time.minutes){
+			var minute_difference = current_time.minutes - last_message_time.minutes;
+			//debug
+			//console.log('minute difference:');	
+			//console.log(minute_difference);
+			if(minute_difference > close_dialog_interval){
+				console.log('Мы не получили никакого бла бла бла');
+				return true;
+			} else {
+				console.log('Not yet');
+				return false;
+			}
+		}
+	} else if(current_time.hours - last_message_time.hours == 1){
+		if(current_time.minutes + 60 >= last_message_time.minutes){
+			var minute_difference = current_time.minutes + 60 - last_message_time.minutes;
+			//debug
+			//console.log('minute difference:');	
+			//console.log(minute_difference);
+			if(minute_difference > close_dialog_interval){
+				console.log('Мы не получили никакого бла бла бла');
+				return true;
+			} else {
+				console.log('Not yet');
+				return false;
+			}
+		}
+	} else {
+		console.log("can't compare hours");
+	}
+}
+function close_dialog_function(){
+	//debug
+	console.log("It's time to close dialog!");
+	var dialog_frame = document.getElementById("chatwnd");
+	dialog_frame.style.border="3px solid red";
+	var textarea = document.getElementById("msgwnd");
+	//console.log(textarea);
+	textarea.value = "Мы не получили от вас никакого сообщения в течение длительного времени. Когда у вас снова возникнут вопросы обращайтесь, мы всегда рады вам помочь. До свидания.";
+}
+var close_divalog_timer_interval = 1 * 60000;
+var close_dialog_timer;
+if (compare_time(get_current_time_object(), last_message_object)){
+	//debug
+	console.log('gotcha');
+	close_dialog_function();
+} else {
+	console.log('set_interval');
+	close_dialog_timer = setInterval(function(){
+		if(compare_time(get_current_time_object(), last_message_object)){
+			close_dialog_function();
+			clearInterval(close_dialog_timer);
+		}
+	}, close_divalog_timer_interval);
 }
 
+//debug
+//console.log(current_date_object);
+//console.log(last_message_object);
