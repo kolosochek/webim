1912b0031ca1772a5d366d33df1a9d5de511f6b4
kolosochek <kolosochek@gmail.com>
2015-11-16 11:05:38 +0500

    refactored a bit, started writing autorespond feature + changed quick-phases set a bit

--- Mchost_chat_help_script.user.js
+++ Mchost_chat_help_script.user.js
@@ -8,9 +8,9 @@
 // ==/UserScript==
 
 // begin
-// inject custom scripts to page
-// taht shit is necessary coz greasemonkey @grant GM_ function breaks global scope
-// GM_xmlhttpRequest required for doing cross-domain ajax requests, which are, by the way, prohibited.
+// inject custom scripts directly to the page
+// that shit is necessary 'coz greasemonkey @grant GM_ function breaks global scope
+// GM_xmlhttpRequest required for doing cross-domain ajax requests, which are, by the way, prohibited in all modern browsers.
 // that's a dirty hack and MUST be refactored
 var script = document.createElement('script');
 script.src = "https://raw.githubusercontent.com/kolosochek/webim/master/complete.js";
@@ -36,196 +36,138 @@ var defaultMchelpSearchAnchor = "!помощь";
 var defaultWhoisSearchAnchor = "!whois";
 // type text in textarea
 textarea.addEventListener('input', function(){
+	// TODO: increase perfomance
   // plhf -> здравствуйте
-  if (this.value == "plh"){
-  	this.value = "Здравствуйте, чем я могу Вам помочь?"
-  }
+  //if (this.value == "plh"){
+  //	this.value = "Здравствуйте, чем я могу Вам помочь?"
+  //}
   // autocomplete feature(.jpg by default) for joxi screenshots
-  var pattern = "http://joxi.ru/";
-  var hash = 14;
-  var extension = ".jpg";
-  var whitespace = ' ';
-  var joxiRegexp = new RegExp("http://joxi.ru/[a-zA-Z0-9]{14}", "g");
-  var joxiRegexpWithExtension = new RegExp("http://joxi.ru/[a-zA-Z0-9.]{17}", "g")
-  // joxi
-  if ((this.value.search(joxiRegexp)+1) && !(this.value.search(joxiRegexpWithExtension)+1)){
-    var patternIndex = this.value.indexOf(pattern)+pattern.length + hash
-    var stringBegining = this.value.substring(0, patternIndex);
-    var stringEnding = this.value.substring(patternIndex);
-    this.value = stringBegining + extension + stringEnding + whitespace;
-  }
-  // qa search
-  var qa_search_regexp = new RegExp(defaultQASearchAnchor+" [а-яА-Яa-zA-Z0-9 ]*!");
-  if (this.value.search(qa_search_regexp)+1){
-    var query = this.value.match(qa_search_regexp)[0];
-    query = query.replace(defaultQASearchAnchor, '').replace('!', '').trim();
-    //debug
-    //console.log(query);
-    GM_xmlhttpRequest({
-        method:     "GET",
-        url:        "https://qa.mchost.ru/pantera/"+query,
-        data:       "",
-        headers:    {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        onload: function (response) {
-        	// debug
-        	//console.log(response.responseText);
-            var qa_links_regexp = new RegExp('<div class="q-line"><a class="q-title" style="" href="[a-zA-Z0-9-\/ ]*">[а-яА-Яa-zA-Z ?]*</a>', 'g');//</a><br>', 'g');
-            var content = '';
-            var links_arr = response.responseText.match(qa_links_regexp);
-			if (links_arr == null) {
-				console.log('Found nothing');
-				textarea.value = defaultQASearchAnchor + ' ';
-			} else {
-	            if (links_arr.length){
-		            for(i=0;i<links_arr.length;i++){
-		                var data = links_arr[i].replace('<div class="q-line"><a class="q-title" style="" href="', '<a class="q-title" href="https://qa.mchost.ru');
-		                data = data.replace("q-title", "b-link");
-		                content+= data;//links_arr[i];            
-		            } 
-		            if(content.length){
-						toggle_modal_window(content);
-					} else {
-						console.log('Found nothing');
-						var content = '<p>Found nothing</p>';
-	        			toggle_modal_window(content);
-					}
-				} 
-			}
-        },
-        ontimeout: function(){
-        	console.log('Timeout');
-        	var content = "<p>Timeout</p>";
-        	toggle_modal_window(content);
-        },
-        // request timeout, 4s for best perfomance
-        timeout: 4000,
-    });
-  }
-  /*
-  // mchost help search
-  var mc_search_regexp = new RegExp(defaultMchelpSearchAnchor+" [а-яА-Яa-zA-Z0-9 ]*!");
-  if (this.value.search(mc_search_regexp)+1){
-    var query = this.value.match(mc_search_regexp)[0];
-    query = query.replace(defaultMchelpSearchAnchor, '').replace('!', '').trim();
-    //debug
-    console.log('query:');
-    console.log(query);
-    GM_xmlhttpRequest({
-        method:     "GET",
-        url:        "http://mchost.ru/search/index.php?bxajaxid=b3d99dd29e4e0053b568d46975f24860&AJAX_CALL=Y&spell=1&q=" + encodeURIComponent(query),//"http://mchost.ru/search/index.php?spell=1&q="+encodeURIComponent(query),
-        data:       "",
-        headers:    {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        onload: function (response) {
-        	// debug
-        	console.log('response text:');
-        	console.log(response.responseText);
-        	// new RegExp('<a href="\/help\/[a-zA-Z0-9-\/ _#=?]*">[a-zA-Zа-яА-Я0-9 _#?]*</a>', '');
-            var mc_links_regexp = new RegExp('<p><a href="\/help\/[a-zA-Z0-9-\/ _#=?]*">[a-zA-Zа-яА-Я0-9 _#?]*</a>', '');//</a>', 'g');//</a><br>', 'g');
-            var content = '';
-            var links_arr = response.responseText.match(mc_links_regexp);
-            //debug
-            console.log('links array:');
-			console.log(links_arr);
-			//console.log(links_arr == null);
-			//console.log(flag);
-			if (links_arr == null) {
-				console.log('Found nothing');
-				textarea.value = defaultMchelpSearchAnchor + ' ';
-				//var content = "<p>Found nothing</p>";
-	        	//toggle_modal_window(content);
-			} else {
-	            if (links_arr.length){
-		            for(i=0;i<links_arr.length;i++){
-		                var data = links_arr[i].replace('<div class="q-line"><a class="q-title" style="" href="', '<a class="q-title" href="https://qa.mchost.ru');
-		                data = data.replace("q-title", "b-link");
-		                content+= data;//links_arr[i];            
-		            } 
-		            // debug
-		            //console.log(content);
-		            //console.log(content == true);
-		            if(content.length){
-						toggle_modal_window(content);
-					} else {
-						console.log('Found nothing');
-						var content = '<p>Found nothing</p>';
-	        			toggle_modal_window(content);
-					}
-				} 
-			}
-        },
-        ontimeout: function(){
-        	console.log('Timeout');
-        	var content = "<p>Timeout</p>";
-        	toggle_modal_window(content);
-        },
-        // request timeout, 4s for best perfomance
-        timeout: 4000,
-    });
-  }*/
-  // whois
-  // TODO: fix regexp to catch things like !whois happy-psn.ru!
-  var whois_search_regexp = new RegExp(defaultWhoisSearchAnchor+" [а-яА-Яa-zA-Z0-9-_. ]*!");
-  if (this.value.search(whois_search_regexp)+1){
-    var query = this.value.match(whois_search_regexp)[0];
-    query = query.replace(defaultWhoisSearchAnchor, '').replace('!', '').trim();
-    //debug
-    console.log('query:');
-    console.log(query);
-    GM_xmlhttpRequest({
-        method:     "GET",
-        url:        "http://whoiz.herokuapp.com/lookup.json?url=" + encodeURIComponent(query),
-        data:       "",
-        headers:    {
-            "Content-Type": "application/json"
-        },
-        onload: function (response) {
-        	var jsonWhois = JSON.parse(response.responseText);
-        	// debug
-        	console.log(jsonWhois);
-            var content = '';
-            var separator = "\n";
-            var ns = jsonWhois.nameservers;
+	  var pattern = "http://joxi.ru/";
+	  var hash = 14;
+	  var extension = ".jpg";
+	  var whitespace = ' ';
+	  var joxiRegexp = new RegExp("http://joxi.ru/[a-zA-Z0-9]{14}", "g");
+	  var joxiRegexpWithExtension = new RegExp("http://joxi.ru/[a-zA-Z0-9.]{17}", "g")
+	  // joxi
+	  if ((this.value.search(joxiRegexp)+1) && !(this.value.search(joxiRegexpWithExtension)+1)){
+	    var patternIndex = this.value.indexOf(pattern)+pattern.length + hash
+	    var stringBegining = this.value.substring(0, patternIndex);
+	    var stringEnding = this.value.substring(patternIndex);
+	    this.value = stringBegining + extension + stringEnding + whitespace;
+	  }
+  //debug
+  // attempt to increase perfomance
+  if((this.value.search('!')+1) && (this.value.length>4)){
+	  // qa search
+	  var qa_search_regexp = new RegExp(defaultQASearchAnchor+" [а-яА-Яa-zA-Z0-9 ]*!");
+	  if (this.value.search(qa_search_regexp)+1){
+	    var query = this.value.match(qa_search_regexp)[0];
+	    query = query.replace(defaultQASearchAnchor, '').replace('!', '').trim();
+	    //debug
+	    //console.log(query);
+	    GM_xmlhttpRequest({
+	        method:     "GET",
+	        url:        "https://qa.mchost.ru/pantera/"+query,
+	        data:       "",
+	        headers:    {
+	            "Content-Type": "application/x-www-form-urlencoded"
+	        },
+	        onload: function (response) {
+	        	// debug
+	        	//console.log(response.responseText);
+	            var qa_links_regexp = new RegExp('<div class="q-line"><a class="q-title" style="" href="[a-zA-Z0-9-\/ ]*">[а-яА-Яa-zA-Z ?]*</a>', 'g');//</a><br>', 'g');
+	            var content = '';
+	            var links_arr = response.responseText.match(qa_links_regexp);
+				if (links_arr == null) {
+					console.log('Found nothing');
+					textarea.value = defaultQASearchAnchor + ' ';
+				} else {
+		            if (links_arr.length){
+			            for(i=0;i<links_arr.length;i++){
+			                var data = links_arr[i].replace('<div class="q-line"><a class="q-title" style="" href="', '<a class="q-title" href="https://qa.mchost.ru');
+			                data = data.replace("q-title", "b-link");
+			                content+= data;//links_arr[i];            
+			            } 
+			            if(content.length){
+							toggle_modal_window(content);
+						} else {
+							console.log('Found nothing');
+							var content = '<p>Found nothing</p>';
+		        			toggle_modal_window(content);
+						}
+					} 
+				}
+	        },
+	        ontimeout: function(){
+	        	console.log('Timeout');
+	        	var content = "<p>Timeout</p>";
+	        	toggle_modal_window(content);
+	        },
+	        // request timeout, 4s for best perfomance
+	        timeout: 4000,
+	    });
+	  }
+	  // whois
+	  // TODO: fix regexp to catch things like !whois happy-psn.ru!
+	  var whois_search_regexp = new RegExp(defaultWhoisSearchAnchor+" [а-яА-Яa-zA-Z0-9-_. ]*!");
+	  if (this.value.search(whois_search_regexp)+1){
+	    var query = this.value.match(whois_search_regexp)[0];
+	    query = query.replace(defaultWhoisSearchAnchor, '').replace('!', '').trim();
+	    //debug
+	    console.log('query:');
+	    console.log(query);
+	    GM_xmlhttpRequest({
+	        method:     "GET",
+	        url:        "http://whoiz.herokuapp.com/lookup.json?url=" + encodeURIComponent(query),
+	        data:       "",
+	        headers:    {
+	            "Content-Type": "application/json"
+	        },
+	        onload: function (response) {
+	        	var jsonWhois = JSON.parse(response.responseText);
+	        	// debug
+	        	console.log(jsonWhois);
+	            var content = '';
+	            var separator = "\n";
+	            var ns = jsonWhois.nameservers;
 
-            // prepend MSHOST WHOIS LINK
-            content+= "http://mchost.ru/whois/?domainName=" + query + separator
+	            // prepend MSHOST WHOIS LINK
+	            content+= "http://mchost.ru/whois/?domainName=" + query + separator
 
-            // NS
-            content+= "NS: ";
-            for (nameserver in ns){
-            	//console.log(ns[nameserver].name);
-            	content+=ns[nameserver].name + whitespace;
-            }
-            content+= separator;            
-        	//register
-        	content+= "Registrar: " + jsonWhois['registrar']['id'] + separator;
-        	//created+expiries+status
-        	content+= "Created: " + jsonWhois['created_on'] + separator;
-        	content+= "Expires: " + jsonWhois['expires_on'] + separator;
-        	content+= "Status: ";
-        	var status = jsonWhois['status']
-        	for(index in status){
-        		content+= status[index] + whitespace;
-        	}
-        	// paste content into textarea
-        	console.log(content);
-        	textarea.value = content;
-        	
-        },
-        ontimeout: function(){
-        	console.log('Timeout');
-        	var content = "<p>Timeout</p>";
-        	toggle_modal_window(content);
-        },
-        // request timeout, 4s for best perfomance
-        timeout: 4000,
-    });
-  }
+	            // NS
+	            content+= "NS: ";
+	            for (nameserver in ns){
+	            	//console.log(ns[nameserver].name);
+	            	content+=ns[nameserver].name + whitespace;
+	            }
+	            content+= separator;            
+	        	//register
+	        	content+= "Registrar: " + jsonWhois['registrar']['id'] + separator;
+	        	//created+expiries+status
+	        	content+= "Created: " + jsonWhois['created_on'] + separator;
+	        	content+= "Expires: " + jsonWhois['expires_on'] + separator;
+	        	content+= "Status: ";
+	        	var status = jsonWhois['status']
+	        	for(index in status){
+	        		content+= status[index] + whitespace;
+	        	}
+	        	// paste content into textarea
+	        	console.log(content);
+	        	textarea.value = content;
+	        	
+	        },
+	        ontimeout: function(){
+	        	console.log('Timeout');
+	        	var content = "<p>Timeout</p>";
+	        	toggle_modal_window(content);
+	        },
+	        // request timeout, 4s for best perfomance
+	        timeout: 4000,
+	    });
+	  }	
+  }  
 });
-
+// modal window functions
 function create_modal_window(content){
 	var modal_window_wrapper = document.createElement("div");
 	modal_window_wrapper.id = "modal_window_wrapper";
@@ -309,5 +251,21 @@ function toggle_modal_window(content){
 	} else {
 		create_modal_window(content);
 	}
+}
+// autoreply functions
+// autoreply 10 min afk
+function get_last_message_timestamp(){
+var content = document.getElementById("chatwnd").contentWindow.document.getElementById("content");
+var span = content.querySelectorAll("span");
+var current_date = new Date();
+var last_event_timestamp;
+  	for (i=span.length-1;i>0;i--){
+  		console.log(i);
+  		if(span[i].className == ""){
+  			console.log(span[i]);
+  			last_event_timestamp = span[i];
+  			//break();
+  		}
+  	}
 }
 
