571fe2957bba9839c472226c510e2f36474a1298
kolosochek <kolosochek@gmail.com>
2015-11-17 12:25:29 +0500

    minor bugfix

--- Mchost_chat_help_script.user.js
+++ Mchost_chat_help_script.user.js
@@ -34,8 +34,6 @@ var defaultExtension = 'jpg';
 var defaultQASearchAnchor = '!найди';
 var defaultMchelpSearchAnchor = "!помощь";
 var defaultWhoisSearchAnchor = "!whois";
-// Set GMT difference with MOSCOW GMT for proper autorespond feature work
-var GMT_difference = "+2"; //TMN +2 
 
 // type text in textarea
 textarea.addEventListener('input', function(){
@@ -102,9 +100,10 @@ textarea.addEventListener('input', function(){
 				}
 	        },
 	        ontimeout: function(){
-	        	console.log('Timeout');
-	        	var content = "<p>Timeout</p>";
-	        	toggle_modal_window(content);
+	        	//console.log('Timeout');
+	        	//var content = "<p>Timeout</p>";
+	        	//toggle_modal_window(content);
+	        	textarea.value = defaultQASearchAnchor + ' -';
 	        },
 	        // request timeout, 4s for best perfomance
 	        timeout: 4000,
@@ -117,8 +116,8 @@ textarea.addEventListener('input', function(){
 	    var query = this.value.match(whois_search_regexp)[0];
 	    query = query.replace(defaultWhoisSearchAnchor, '').replace('!', '').trim();
 	    //debug
-	    console.log('query:');
-	    console.log(query);
+	    //console.log('query:');
+	    //console.log(query);
 	    GM_xmlhttpRequest({
 	        method:     "GET",
 	        url:        "http://whoiz.herokuapp.com/lookup.json?url=" + encodeURIComponent(query),
@@ -129,7 +128,7 @@ textarea.addEventListener('input', function(){
 	        onload: function (response) {
 	        	var jsonWhois = JSON.parse(response.responseText);
 	        	// debug
-	        	console.log(jsonWhois);
+	        	//console.log(jsonWhois);
 	            var content = '';
 	            var separator = "\n";
 	            var ns = jsonWhois.nameservers;
@@ -161,8 +160,9 @@ textarea.addEventListener('input', function(){
 	        },
 	        ontimeout: function(){
 	        	console.log('Timeout');
-	        	var content = "<p>Timeout</p>";
-	        	toggle_modal_window(content);
+	        	//var content = "<p>Timeout</p>";
+	        	//toggle_modal_window(content);
+	        	textarea.value = defaultWhoisSearchAnchor + ' -';
 	        },
 	        // request timeout, 4s for best perfomance
 	        timeout: 4000,
@@ -255,11 +255,6 @@ function toggle_modal_window(content){
 		create_modal_window(content);
 	}
 }
-
-
-
-
-
 // autoreply functions
 // autoreply 10 min afk
 var GMT_difference = "+2"; //TMN +2 
@@ -268,113 +263,110 @@ var close_dialog_interval = 10;
 var close_divalog_timer_interval = 1 * 60000;
 
 function get_last_message_timestamp(){
-	var content = document.getElementById("chatwnd").contentWindow.document.getElementById("content");
-	var span = content.querySelectorAll("span");
-	var last_event_timestamp;
-  	for (i=span.length-1;i>0;i--){
-  		if(span[i].className == ""){
-  			last_event_timestamp = span[i].textContent;
-  			break;
-  		}
-  	}
-	if (typeof(last_event_timestamp) == 'string'){
-		last_event_timestamp = last_event_timestamp.split(':');
-		if (last_event_timestamp.length == 3){
-			var object_params = {
-				'hours': parseInt(last_event_timestamp[0]),
-				'minutes': parseInt(last_event_timestamp[1]),
-				'seconds': parseInt(last_event_timestamp[2]),
-			}
-			return new Object(object_params);
-		} else {
-			console.log('wrong last message timestamp split');
-			console.log(last_event_timestamp);
-		}
-	} else {
-		return false	
-	}
+    //debug
+    var content = document.getElementById("chatwnd").contentWindow.document.getElementById("content");
+    var span = content.querySelectorAll("span");
+    var last_event_timestamp;
+    for (i=span.length-1;i>0;i--){
+        if(span[i].className == ""){
+            last_event_timestamp = span[i].textContent;
+            break;
+        }
+    }
+    if (typeof(last_event_timestamp) == 'string'){
+        last_event_timestamp = last_event_timestamp.split(':');
+        if (last_event_timestamp.length == 3){
+            var object_params = {
+                'hours': parseInt(last_event_timestamp[0]),
+                'minutes': parseInt(last_event_timestamp[1]),
+                'seconds': parseInt(last_event_timestamp[2]),
+            }
+            return new Object(object_params);
+        } else {
+            console.log('wrong last message timestamp split');
+            console.log(last_event_timestamp);
+        }
+    } else {
+        return false    
+    }
 }
 //debug
 //var last_message_object = get_last_message_timestamp();
 
 function get_current_time_object(){
-	var current_date = new Date();	
-	var current_date_object = {
-		'hours': 0,//parseInt(current_date.getHours())-GMT_difference,
-		'minutes': parseInt(current_date.getMinutes()),
-		'seconds': parseInt(current_date.getSeconds()),
-	}
-	if (GMT_difference.search(/\+/g)+1){
-		current_date_object.hours = current_date.getHours() - parseInt(GMT_difference);
-	} else if(GMT_difference.search('-')+1){
-		current_date_object.hours = current_date.getHours() + parseInt(GMT_difference);
-	} else {
-		console.log("can't set current hours value with GMT difference");
-		current_date_object.hours = 0;
-	}
-	return current_date_object
+    var current_date = new Date();  
+    var current_date_object = {
+        'hours': 0,//parseInt(current_date.getHours())-GMT_difference,
+        'minutes': parseInt(current_date.getMinutes()),
+        'seconds': parseInt(current_date.getSeconds()),
+    }
+    if (GMT_difference.search(/\+/g)+1){
+        current_date_object.hours = current_date.getHours() - parseInt(GMT_difference);
+    } else if(GMT_difference.search('-')+1){
+        current_date_object.hours = current_date.getHours() + parseInt(GMT_difference);
+    } else {
+        console.log("can't set current hours value with GMT difference");
+        current_date_object.hours = 0;
+    }
+    return current_date_object
 }
 function compare_time(current_time, last_message_time){
-	// compare hours
-	if (current_time.hours == last_message_time.hours){
-		console.log('hours are matching');
-		// compare minutes
-		if(current_time.minutes >= last_message_time.minutes){
-			var minute_difference = current_time.minutes - last_message_time.minutes;
-			//debug
-			//console.log('minute difference:');	
-			//console.log(minute_difference);
-			if(minute_difference > close_dialog_interval){
-				console.log('Мы не получили никакого бла бла бла');
-				return true;
-			} else {
-				console.log('Not yet');
-				return false;
-			}
-		}
-	} else if(current_time.hours - last_message_time.hours == 1){
-		if(current_time.minutes + 60 >= last_message_time.minutes){
-			var minute_difference = current_time.minutes + 60 - last_message_time.minutes;
-			//debug
-			//console.log('minute difference:');	
-			//console.log(minute_difference);
-			if(minute_difference > close_dialog_interval){
-				console.log('Close dialog: true');
-				return true;
-			} else {
-				console.log('Close dialog: not yet');
-				return false;
-			}
-		}
-	} else {
-		console.log("can't compare hours");
+	function check_difference(minute_difference){
+		if(minute_difference > close_dialog_interval){
+                console.log('Close dialog: true');
+                return true;
+            } else {
+                console.log('Close dialog: not yet');
+                return false;
+            }	
 	}
+    // compare hours
+    if (current_time.hours == last_message_time.hours){
+    	//debug
+        //console.log('hours are matching');
+        // compare minutes
+        if(current_time.minutes >= last_message_time.minutes){
+            var minute_difference = current_time.minutes - last_message_time.minutes;
+            return check_difference(minute_difference);
+        }
+    } else if(current_time.hours - last_message_time.hours == 1){
+        if(current_time.minutes + 60 >= last_message_time.minutes){
+            var minute_difference = current_time.minutes + 60 - last_message_time.minutes;            
+            return check_difference(minute_difference);
+        }
+    } else {
+        console.log("can't compare hours");
+    }
 }
 // function fired when it's time to close the dialog
 function close_dialog_function(){
-	//debug
-	console.log("It's time to close dialog!");
-	var dialog_frame = document.getElementById("chatwnd");
-	dialog_frame.style.border="3px solid red";
-	var textarea = document.getElementById("msgwnd");
-	//console.log(textarea);
-	textarea.value = "Мы не получили от вас никакого сообщения в течение длительного времени. Когда у вас снова возникнут вопросы обращайтесь, мы всегда рады вам помочь. До свидания.";
+    //debug
+    console.log("It's time to close dialog!");
+    var dialog_frame = document.getElementById("chatwnd");
+    dialog_frame.style.border="3px solid red";
+    var textarea = document.getElementById("msgwnd");
+    //console.log(textarea);
+    textarea.value = "Мы не получили от вас никакого сообщения в течение длительного времени. Когда у вас снова возникнут вопросы обращайтесь, мы всегда рады вам помочь. До свидания.";
 }
 // create a timer to check
 var close_dialog_timer;
-// initial check, f.e. after dialog page refresh or reload
-if (compare_time(get_current_time_object(), get_last_message_timestamp())){
-	close_dialog_function();
-} else {
-	// debug
-	console.log('set_interval');
-	close_dialog_timer = setInterval(function(){
-		if(compare_time(get_current_time_object(), get_last_message_timestamp())){
-			close_dialog_function();
-			clearInterval(close_dialog_timer);
-		}
-	}, close_divalog_timer_interval);
-}
+
+// initial check, f.e. after dialog page refresh or reload, have 5 sec delay because dialog history are injected in DOM
+// by ajax so the script can't get last message timestamp right on DOM ready.
+setTimeout(function(){	
+	if (compare_time(get_current_time_object(), get_last_message_timestamp())){
+    close_dialog_function();
+	} else {
+	    // debug
+	    //console.log('set_interval');
+	    close_dialog_timer = setInterval(function(){
+	        if(compare_time(get_current_time_object(), get_last_message_timestamp())){
+	            close_dialog_function();
+	            clearInterval(close_dialog_timer);
+	        }
+	    }, close_divalog_timer_interval);
+	}
+}, 2000);
 
 //debug
 //console.log(current_date_object);
